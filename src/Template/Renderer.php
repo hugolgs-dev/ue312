9837cf76713080f713b5412cb32b24f3cdc2fab6
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-06 12:51:23 +0100

    init

---
+++ src/Template/Renderer.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Framework312\Template;
+
+interface Renderer
+{
+    public function render(mixed $data, string $template): string;
+    public function register(string $tag);
+}
+
+?>
