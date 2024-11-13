b74067cd0c0231bef4e0fe228e801a1e18cdea8e
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-13 14:55:12 +0100

    ajout fichiers views

---
+++ src/Router/View/JSONView.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Framework312\Router\View;
+
+use Framework312\Router\Exception as RouterException;
+use Framework312\Router\Request;
+use Symfony\Component\HttpFoundation\Response;
