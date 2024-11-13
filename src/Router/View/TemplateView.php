9a0eb684085b2a43d29e2a971428b75500631e38
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-13 20:08:46 +0100

    suppr fichiers viewers

--- src/Router/View/TemplateView.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Framework312\Router\View;
-
-use Framework312\Router\Exception as RouterException;
-use Framework312\Router\Request;
-use Symfony\Component\HttpFoundation\Response;
