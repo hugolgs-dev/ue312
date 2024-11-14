74d55f0db87122f0c74d7f3e209537182354fc2c
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-14 15:22:00 +0100

    ajout classes views

---
+++ src/Router/View/TemplateView.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Framework312\Router\View;
+
+use Framework312\Router\Request;
+use Framework312\Router\View\BaseView;
+use Symfony\Component\HttpFoundation\Response;
+
+class TemplateView extends BaseView
+{
+
+    static public function use_template(): bool
+    {
+        // TODO: Implement use_template() method.
+    }
+
+    public function render(Request $request): Response
+    {
+        // TODO: Implement render() method.
+    }
+}
