8104d761d30d4038a9aa05da91abc9fd6d3bc9a2
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-21 14:46:02 +0100

    commentaires HTMLViewTest.php & JSONViewTest.php

--- tests/Router/View/HTMLViewTest.php
+++ tests/Router/View/HTMLViewTest.php
@@ -8,23 +8,33 @@ use Symfony\Component\HttpFoundation\Response;
 class HTMLViewTest extends TestCase {
     public function testRenderReturnHtmlResponse(): void
     {
+        // On crée une instance de requête
         $request = new Request();
 
+        // On crée une vue HTMLView, avec une méthode get pour le test
         $view = new class() extends HTMLView {
             protected function get(Request $request): string
             {
+                // Contenu de la vue crée pour le test
                 return '<html><body>Ça fonctionne!</body></html>';
             }
         };
 
+        // On appelle la méthode "render"
         $response = $view->render($request);
 
+        /* Assertions */
+
+        // On vérifie que la réponse est une instance de Response
         $this->assertInstanceOf(Response::class, $response);
 
+        // On vérifie le type de la réponse, qui doit être text/html ici
         $this->assertEquals('text/html', $response->headers->get('Content-Type'));
 
+        // On vérifie que le contenu de la réponse est valide par rapport au format attendu
         $this->assertEquals('<html><body>Ça fonctionne!</body></html>', $response->getContent());
 
+        // On vérifie que le statut de la requête HTTP est bien 200
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
     }
 }
--- tests/Router/View/JSONViewTest.php
+++ tests/Router/View/JSONViewTest.php
@@ -8,24 +8,37 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class JSONViewTest extends TestCase {
     public function testRenderReturnJsonResponse(): void
     {
+        // On crée une instance de requête
         $request = new Request();
 
+        // On crée une vue JSONView, avec une méthode get pour le test
         $view = new class() extends JSONView {
             protected function get(Request $request): array
             {
+                // Contenu de la vue crée pour le test
                 return ['status' => 'ok', 'message' => 'ok'];
             }
         };
 
+        // On appelle la méthode "render"
         $response = $view->render($request);
 
+        /* Asserions */
+
+        // On vérifie que la réponse est bien une instance de Response
         $this->assertInstanceOf(JsonResponse::class, $response);
 
+        // On vérifie le type de la réponse, qui doit être application/json ici
         $this->assertEquals('application/json', $response->headers->get('Content-Type'));
 
+        // On vérifie le contenu de la réponse en 2 étapes :
+
+            // on traduit la réponse, sous forme de tableau, en chaîne JSON avec json_encode
         $expectedContent = json_encode(['status' => 'ok', 'message' => 'ok']);
+            // on vérifie que le contenu retourné est correctement convertie en JSON, donc valide avec le format de données attendu
         $this->assertEquals($expectedContent, $response->getContent());
 
+        // On vérifie que le statut de la requête est bien 200
         $this->assertEquals(200, $response->getStatusCode());
 
     }
