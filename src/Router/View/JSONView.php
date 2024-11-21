65f542e0ec4df5c8cb49de6a709ddb3cfa9e6de0
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-21 14:31:07 +0100

    commentaires HTMLView.php & JSONView.php

--- src/Router/View/JSONView.php
+++ src/Router/View/JSONView.php
@@ -9,23 +9,30 @@ use Symfony\Component\HttpFoundation\Response;
 
 class JSONView extends BaseView
 {
-
+    // on indique si cette vue utilise ou non un moteur de template (Twig)
     static public function use_template(): bool
     {
-        // return false car la vue n'utilise pas TWIG
+        // 'return false' car la vue n'a pas besoin d'utiliser Twig
         return false;
     }
 
+    // on génère et on renvoie une réponse HTTP avec un contenu JSON
     public function render(Request $request): Response
     {
+        // On récupère la méthode HTTP utilisée dans la requête génèrée
         $method = strtolower($request->getMethod());
 
+        // On vérifie que la méthode utilisée existe bien. Si c'est le cas, on pas à la suite
         if(!method_exists($this, $method)) {
+            // Sinon, cela signifie que la méthode n'est pas implémentée ---> on lève une exception
             throw new \BadMethodCallException("La méthode HTTP [$method] n'existe pas.");
         }
 
+        // On appelle la méthode correspondante à la requête et on récupère son contenu
         $data = $this->$method($request);
 
+        // On retourne une réponse JSON avec les données dans ce format et un type 'application/json'
+        // => ce type est déjà géré par 'JsonResponse', donc pas besoin de le préciser
         return new JsonResponse($data, Response::HTTP_OK);
     }
 }
