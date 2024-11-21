8ec5337525952dec7906567e70c58e26899efca3
Hugo Lignères <hugo.ligneres@orange.fr>
2024-11-21 15:06:30 +0100

    micro fixes HTMLView.php & JSONView.php

--- src/Router/View/HTMLView.php
+++ src/Router/View/HTMLView.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php declare(strict_types=1);
 
 namespace Framework312\Router\View;
@@ -6,14 +5,8 @@ namespace Framework312\Router\View;
 use Framework312\Router\Exception as RouterException;
 use Framework312\Router\Request;
 use Symfony\Component\HttpFoundation\Response;
-=======
-<?php
-
-namespace Framework312\Router\View;
-
-use Framework312\Router\Request;
 use Framework312\Router\View\BaseView;
-use Symfony\Component\HttpFoundation\Response;
+
 
 class HTMLView extends BaseView
 {
@@ -43,4 +36,3 @@ class HTMLView extends BaseView
         return new Response($data, Response::HTTP_OK, ['Content-Type' => 'text/html']);
     }
 }
->>>>>>> 083d0bc0bff4052680f242d7f3908543fe4466f0
--- src/Router/View/JSONView.php
+++ src/Router/View/JSONView.php
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 <?php declare(strict_types=1);
 
 namespace Framework312\Router\View;
 
 use Framework312\Router\Exception as RouterException;
-use Framework312\Router\Request;
-use Symfony\Component\HttpFoundation\Response;
-=======
-<?php
-
-namespace Framework312\Router\View;
-
 use Framework312\Router\Request;
 use Framework312\Router\View\BaseView;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -45,4 +37,4 @@ class JSONView extends BaseView
         return new JsonResponse($data, Response::HTTP_OK);
     }
 }
->>>>>>> 083d0bc0bff4052680f242d7f3908543fe4466f0
+
